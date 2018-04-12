7dbeb3198f8ebc1c2fa966620b7d0df1f308e1bb
ThidaC <chheang.thida@gmail.com>
2018-04-12 23:16:08 +0200

    modification index.php, style.css, script.js ; ajout des fichiers MarkerCluster

--- include/js/script.js
+++ include/js/script.js
@@ -1,19 +1,8 @@
-// When the user scrolls down 1000px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope) {
+	$scope.name = $('#query').val();
+});
 
-function scrollFunction() {
-    if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
-        document.getElementById("myBtn").style.display = "block";
-    } else {
-        document.getElementById("myBtn").style.display = "none";
-    }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
 
 $(document).ready(function() {
 
@@ -23,7 +12,6 @@ $(document).ready(function() {
 		$(this).toggleClass('active');
 		
 		$('#nav').toggle();
-		//$('#nav').toggleClass('act');
     });
 	
 });
